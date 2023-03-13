235bfc65bfd9de93188914866c5b0832a3dec3ef
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-03-13 12:50:43 +0200

    Define a default sort order

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -6,9 +6,17 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
+    # Added datetime fields
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    
+    # Define a default sort order
+    class Meta:
+        # Blog posts are usually displayed in reverse chronological order(from newest to oldest).
+        # Indicate descending order by using a hyphen before the field name, -publish.
+        # Posts will be returned in reverse chronological order by default.
+        ordering = ['-publish']
 
     def __str__(self):
         return self.title
