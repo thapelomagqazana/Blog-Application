35a48af45b4ac401bd3aefe02b209ca62e327c74
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-03-13 16:31:13 +0200

    Created the django admin site and Added models to the site with a few customizations

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig', # Activate the application
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
