b50abae22ce61a1f4a03efae0c0f508fcd2f72bc
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-03-14 11:40:44 +0200

    Created views for published posts and single blog post

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
+# Customize the admin interface for the 'Post' model by specifying which fields to display, how to filter and search instances,
+# and how to handle certain fields like 'slug' and 'author'
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'author', 'publish', 'status']
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import Http404
 
 # Create your views here.
+
+# This view function retrieves all published blog posts and renders them using a template.
+def post_list(request):
+    posts = Post.published.all()
+    return render(
+        request,
+        'blog/post/list.html',
+        {'posts': posts}
+    )
+
+# This view function retrieves a single blog post based on its ID and renders it using a template.
+def post_detail(request,id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("No Posts found.")
+    
+    return render(
+        request,
+        'blog/post/detail.html',
+        {'post': post}
+    )
